5fbdaf09bcd51704204aa58e08f2e35ae2a26f68
PC <lucaslorran2016@gmail.com>
2024-11-01 11:57:33 -0300

    feat: atualiza código de estudo

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2.5;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(numeroCurto2);
+        
+    }
+}
